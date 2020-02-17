f4321773f401f713334afd027e4ba0eda74c825d
kovchin <kovchin@mail.ru>
2020-02-17 23:12:57 +0700

    17.02.2020

--- application/core/Controller.php
+++ application/core/Controller.php
@@ -7,28 +7,28 @@ use application\core\View;
 abstract class Controller
 {
 
-    public $route;
-    public $view;
+	public $route;
+	public $view;
 
-    public function __construct($route)
-    {
-        $this->route = $route;
-        $this->checkAcl();
-        $this->view = new View($route);
-        $this->model = $this->LoadModel($route['controller']);
-    }
+	public function __construct($route)
+	{
+		$this->route = $route;
+		$this->checkAcl();
+		$this->view = new View($route);
+		$this->model = $this->LoadModel($route['controller']);
+	}
 
-    public function LoadModel($name)
-    {
-        $path = 'application\models\\' . ucfirst($name);
-        if (class_exists($path)) {
-            return new $path;
-        }
-    }
+	public function LoadModel($name)
+	{
+		$path = 'application\models\\' . ucfirst($name);
+		if (class_exists($path)) {
+			return new $path;
+		}
+	}
 
-    public function checkAcl()
-    {
-        $acl = require 'application\acl\\' . $this->route['controller'] . '.php';
-        debug($acl);
-    }
+	public function checkAcl()
+	{
+		$acl = require 'application\acl\\' . $this->route['controller'] . '.php';
+		debug($acl);
+	}
 };
